b9d481585eaf580b18a2e66dedecd1d117426c1a
pckhib <patrickhieber92@gmail.com>
2018-04-03 20:40:13 +0200

    feat(tasks): filter tasks in Kanban view

--- app/scripts/controllers/ApplicationController.js
+++ app/scripts/controllers/ApplicationController.js
@@ -1,8 +1,10 @@
 'use strict';
 
 angular.module('mpk').controller('ApplicationController', 
-	function ApplicationController($scope, $window, kanbanRepository, themesProvider, $routeParams, $location, cloudService) {
+	function ApplicationController($scope, $rootScope, $window, kanbanRepository, themesProvider, $routeParams, $location, cloudService) {
 	$scope.colorOptions = ['FFFFFF','DBDBDB','FFB5B5', 'FF9E9E', 'FCC7FC', 'FC9AFB', 'CCD0FC', '989FFA', 'CFFAFC', '9EFAFF', '94D6FF','C1F7C2', 'A2FCA3', 'FAFCD2', 'FAFFA1', 'FCE4D4', 'FCC19D'];
+
+	$rootScope.taskFilter = '';
 
 	// <-------- Handling different events in this block ---------------> //
 	$scope.$on('NewKanbanAdded', function(){
